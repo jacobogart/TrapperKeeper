dc7364be0c6f441f8f6d95b1873bf044b9a1e500
Max Silver <Maxbsilver@gmail.com>
2019-05-20 16:53:37 -0600

    add initial banner styling

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 
 import { combineReducers } from "redux";
 import { questsReducer } from "./questsReducer";
+import { userReducer } from "./userReducer";
 
 const rootReducer = combineReducers({
-  quests: questsReducer
+  quests: questsReducer,
+  user: userReducer
 });
 
 export default rootReducer;
