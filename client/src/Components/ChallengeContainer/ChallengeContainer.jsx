7fcbfe48d6463647369158588940479266dfaec5
Jacob Bogart <45608610+jacobogart@users.noreply.github.com>
2019-05-22 11:41:58 -0600

    Revert "Add backend server"

--- client/src/Components/ChallengeContainer/ChallengeContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from './node_modules/ react';
-
-export class ChallengeContainer extends Component {
-	render() {
-		return <div />;
-	}
-}
-
-export default ChallengeContainer;
--- client/src/reducers/questsReducer.js
+++
@@ -1,6 +0,0 @@
-export const questsReducer = (state = [], action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
-};
